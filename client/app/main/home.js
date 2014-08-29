dddc8d1a693d63f5384e03621c9a03559f13e016
Geoffrey Abdallah <gabdallah222@gmail.com>
2014-08-28 17:28:07 -0700

    Added home template and map

---
+++ client/app/main/home.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('yokelApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/home/home.html',
+        controller: 'HomeController'
+      });
+  });
