f41aa3098b5690c01ef934c7c390e2a9c6732255
Stefanie Contreras <stefsnow@gmail.com>
2014-08-28 17:20:22 -0700

    create basic template for business

---
+++ client/app/business/business.controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('yokelApp')
+  .controller('BusinessController', function ($scope, $http) {
+
+  })
+
+  .controller('MapController', function($scope, $http){
+
+  })
+  .controller('ReviewController', function($scope, $http){
+    
+  });
