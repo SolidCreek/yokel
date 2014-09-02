496f77a0cce857c0f0f02a5002764ce02e4421f1
Geoffrey Abdallah <gabdallah222@gmail.com>
2014-09-02 17:28:26 -0500

    Added comments and style guide changes to Auth.js

--- client/app/user/Auth.js
+++ client/app/user/Auth.js
@@ -3,58 +3,68 @@ angular.module('yokelApp')
 .controller('AuthController', function($scope, Auth, $window, $location){
   $scope.user = {};
 
-  $scope.signin = function () {
+
+  //Sets token in user's local storage
+  var setToken = function(token){
+    $window.localStorage.setItem("TOKEN NAME", token);
+    $location.path('/users');
+  };
+
+  //Returns the token property on response object served up by server
+  var returnResponseToken = function(response){
+    return response.data.token;
+  };
+
+  //Signs user in and sets token
+  $scope.signin = function(){
     Auth.signin($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem("TOKEN NAME", token);
-        $location.path('/users');
-      })
-      .catch(function (error) {
+      .then(setToken(token))
+      .catch(function(error){
         console.error(error);
       });
   };
 
-  $scope.signup = function () {
+  //Signs user up and sets token
+  $scope.signup = function(){
     Auth.signup($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem("TOKEN NAME", token);
-        $location.path('/users');
-      })
-      .catch(function (error) {
+      .then(setToken(token))
+      .catch(function(error){
         console.error(error);
       });
   };
 })
 
 .factory('Auth', function($http, $location, $window){
+
+  //Signs user in and returns token
   var signin = function(user){
     return $http({
       method: 'POST',
       url: '/api/users/signin',
       data: user
-    }).then(function (resp) {
-      return resp.data.token;
     })
-  }
+      .then(returnResponseToken(response));
+  };
 
+  //Signs user up and returns token
   var signup = function(user){
     return $http({
       method: 'POST',
       url: '/api/users/signup',
       data: user
-    }).then(function (resp) {
-      return resp.data.token;
     })
-  }
+      .then(returnResponseToken(response));  
+  };
 
+  //Removes token from local storage and redirects user to home page
   var signout = function(user){
     $window.localStorage.removeItem("TOKEN");
     $location.path('/');
-  }
+  };
 
   return {
     signin: signin,
     signup: signup,
     logout: logout
-  }
+  };
 });
