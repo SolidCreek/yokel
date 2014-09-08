6089b8ada246237af582f097c09fcc16da19f698
kokobeens <collinkokotas@gmail.com>
2014-09-08 12:30:10 -0700

    Businesses represented by markers on map

--- client/app/map.js
+++
@@ -1,7 +0,0 @@
-// angular.module('app.map', ['google-maps', 'yokelApp'])
-//   .controller('mapController', function($scope) {
-//     $scope.markers = {};
-//     $scope.map = {center: {latitude: locate()[0], longitude: locate()[1]}, zoom: 13 };
-//     $scope.markers = getBusinessPages
-//     $scope.$watch('markers', function()
-//   });
